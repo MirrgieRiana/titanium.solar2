3f58cd4bfcab17d6cfadbf230da7ac4772d4aad1
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-01-29 08:44:02 +0900

    analyzer: chain -> packet

--- titanium.solar2.libs.analyze/src/main/java/titanium/solar2/libs/analyze/mountainlisteners/chain/IChainListener.java
+++
@@ -1,8 +0,0 @@
-package titanium.solar2.libs.analyze.mountainlisteners.chain;
-
-public interface IChainListener
-{
-
-	public void onChain(Chain chain);
-
-}
---
+++ titanium.solar2.libs.analyze/src/main/java/titanium/solar2/libs/analyze/packetdetectors/PacketDetectorBase.java
@@ -0,0 +1,10 @@
+package titanium.solar2.libs.analyze.packetdetectors;
+
+import titanium.solar2.libs.analyze.IPulseListener;
+import titanium.solar2.libs.analyze.ItemDetectorBase;
+import titanium.solar2.libs.analyze.Packet;
+
+public abstract class PacketDetectorBase extends ItemDetectorBase<Packet> implements IPulseListener
+{
+
+}
