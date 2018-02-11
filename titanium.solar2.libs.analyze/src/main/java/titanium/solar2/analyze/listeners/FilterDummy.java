2c4bd23536c57a301311b67c4b49fbfa0b78a956
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-02-11 16:01:40 +0900

    analyzer: add: FilterDummy

---
+++ titanium.solar2.libs.analyze/src/main/java/titanium/solar2/analyze/listeners/FilterDummy.java
@@ -0,0 +1,15 @@
+package titanium.solar2.analyze.listeners;
+
+import mirrg.lithium.struct.Struct1;
+import titanium.solar2.libs.analyze.IFilter;
+
+public class FilterDummy implements IFilter
+{
+
+	@Override
+	public void processData(double[] buffer, int length, Struct1<Double> sOffset)
+	{
+
+	}
+
+}
